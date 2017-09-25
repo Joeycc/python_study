56cab17ee54a113b62046a17df7e11f9de933697
joeycc <chenwanchen1986@163.com>
2017-09-25 21:24:40 +0800

    django test

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    timestamp = models.DateTimeField()
+
+class UserInfo(models.Model):
+    user = models.CharField(max_length=32)
+    pwd = models.CharField(max_length=32)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
+from blog import models
 # Create your views here.
+
+
+def index(request):
+    #return HttpResponse('hello world!')
+    userlist = []
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        print(username, password)
+        models.UserInfo.objects.create(user=username, pwd=password)
+
+    userlist = models.UserInfo.objects.all()
+    return render(request, 'index.html', {'data': userlist})
